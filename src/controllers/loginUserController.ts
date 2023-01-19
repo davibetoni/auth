87e910ae16d7840f96e7d631b2c968e56ad2644d
davibetoni <davibetoni@gmail.com>
2023-01-18 21:32:21 -0300

    initial login

---
+++ src/controllers/loginUserController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import { sign } from "jsonwebtoken";
+import { authenticateRepository } from "../repositories/authenticateRepository";
+
+export async function loginUserController(req: Request, res: Response) {
+  const { email, password } = req.body;
+
+  const userId = await authenticateRepository(res, email, password);
+
+  const accessToken = sign({ id: userId }, "access_secret", {
+    expiresIn: 60 * 60,
+  });
+
+  res.send({
+    message: "success",
+    token: accessToken,
+  });
+}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { registerUserController } from "../controllers/createUserController";
+import { loginUserController } from "../controllers/loginUserController";
 
 export const routes = (router: Router) => {
     router.post('/user/register', registerUserController)
+    router.post('/user/login', loginUserController)
 }
