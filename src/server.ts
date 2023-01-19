f97db536e7e0c34cf7d8317308fef434c1e1462d
davibetoni <davibetoni@gmail.com>
2023-01-19 12:14:41 -0300

    handling errors

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,25 @@
-import express from "express";
-import "./database"
-import { routes } from './routes';
+import express, { NextFunction, Request, Response } from "express";
+import "./database";
+import { routes } from "./routes";
+import "express-async-errors";
 
 const app = express();
 
 app.use(express.json());
 routes(app);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+      return response.status(400).json({
+        message: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      message: "Internal server error",
+    });
+  }
+);
 
 app.listen(4001, () => console.log("Auth server is running."));
